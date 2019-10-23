714ae8649a2dfb05c6d985d0f230a1203a3cc6f8
asian-code <52612730+asian-code@users.noreply.github.com>
2019-10-23 19:14:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -42,17 +42,26 @@ br = '\33[108m'
 brown = '\33[33m'
 bwhite = '\33[107'
 
+
 # updates main menu options based on input
+
+
 def isSelected(value):
     if (value in user_options):
         return "X"
     return value
 
+
 # displays [value], string for main menu
+
+
 def opt(value, string):
     return r + "  [" + green + str(value) + r + "] " + string
 
-# displays the logo 
+
+# displays the logo
+
+
 def welcome():
     os.system("clear " * 5)
     print(r + """
@@ -72,70 +81,75 @@ def welcome():
 
   """ + r + "1st Developer: " + green + "lin8x" + r + " - " + ul + lcyan + "www.github.com/lin8x " + r +
           "\n  2nd Developer: " + green + "asian-code" + r + " - " + ul + lcyan + "www.github.com/asian-code" + r +
-          "\n  Select the options you would like to use, then type 'start' to continue:\n\n"+opt(isSelected(1), "Make a Desktop Application\n") +
+          "\n  Select the options you would like to use, then type 'start' to continue:\n\n" + opt(isSelected(1),
+                                                                                                   "Make a Desktop Application\n") +
           opt(isSelected(2), "Make a .deb file (DOESN'T WORK ON NFTS!)\n") +
           opt(isSelected(3), "Compile your name into a .tar.xz/.zip file\n"))
 
 
 def run():
-    #bash file name
-    name = input(opt("*","Enter app name : "))
-    if(name == ""):
+    # bash file name
+    name = input(opt("*", "Enter app name : "))
+    if (name == ""):
         name = "projects"
     # folder location
-    directory = input(opt("*","enter your projects name directory (~/mystuff): "))
-    if(directory == ""):
+    directory = input(
+        opt("*", "enter your projects name directory (~/mystuff): "))
+    if (directory == ""):
         directory = "~/mystuff"
     # script to run/ write to bash file
-    script = input(opt("*","Please enter your script's directory (~/mystuff.py): "))
-    if(script == ""):
+    script = input(
+        opt("*", "Please enter your script's directory (~/mystuff.py): "))
+    if (script == ""):
         script = "~/mystuff.py"
     # need to talk to DANISGAY about what its purpose
-    runscript = input(opt("*","Please enter the command in the terminal to run your tool\n (python3 mystuff.py)"))
+    runscript = input(opt(
+        "*", "Please enter the command in the terminal to run your tool\n (python3 mystuff.py)"))
 
-    if(1 in user_options):
-        # get location of icon image 
-        image = input(opt("*","Please enter your image/logo's directory (home/mystuff.ico): "))
-        if(image == ""):
+    if (1 in user_options):
+        # get location of icon image
+        image = input(
+            opt("*", "Please enter your image/logo's directory (home/mystuff.ico): "))
+        if (image == ""):
             image = "~/mystuff.ico"
         # determine if terminal runs when launching through desktop icon
-        terminal = input(opt("*", "Is your software run on a terminal? (y/n): "))
+        terminal = input(
+            opt("*", "Is your software run on a terminal? (y/n): "))
         terminal = (terminal.lower() == "y" or terminal.lower() == "yes")
         try:
             # NEED TO CHECK FOR USER INPUT TO INCREASE APPLICATION SECURITY
             # app is using terminal commands which could be injected
-            #make sure name and dir input are characters only
-            projectdir = name+"dir"
+            # make sure name and dir input are characters only
+            projectdir = name + "dir"
 
             # makes the directory file
-            #os.system("mkdir " + directory)
-            #opt(red + "+", "Made " + directory)
+            # os.system("mkdir " + directory)
+            # opt(red + "+", "Made " + directory)
 
-            # makes the parentfolder with subfolder 
+            # makes the parentfolder with subfolder (but whats the point of having a subfolder?)
             os.system("mkdir -p " + directory + "/" + projectdir)
             print(opt(red + "+", "Made " + directory + "/" + name))
 
-            # copies the script file into the directory 
+            # copies the script file into the directory
             os.system("cp " + script + " " + directory + "/" + projectdir)
-            opt(red + "+", "Moved " + script +
-                " to " + directory + "/" + projectdir)
-
+            print(opt(red + "+", "Moved " + script +" to " + directory + "/" + projectdir))
+            # copies the icon file to subfolder
             os.system("cp " + image + " " + directory + "/" + projectdir)
-            opt(red + "+", "Moved " + image +
-                " to " + directory + "/" + projectdir)
+            print(opt(red + "+", "Moved " + image +" to " + directory + "/" + projectdir))
 
-            os.system("touch " + name)
-            opt(red + "+", "Made the batch file called " + name)
+            #create bash file
+            print(opt(red + "+", "Made the batch file called " + name))
 
             file = open(name, "a")
             file.write(runscript)
             file.close()
-
+            #
             os.system("mv " + name + " " + directory)
             opt(red + "+", "Moved " + name + " to " + directory)
 
             opt(red + "+", "Creating " + name + ".desktop (PRESS CNTRL + C)")
-            os.system("cat > " + name + ".desktop")
+            # DISGUSTING LIN8X CODE
+            #os.system("cat > " + name + ".desktop")
             print()
             opt(red + "+", "Created " + name + ".desktop")
 
@@ -150,33 +164,34 @@ def run():
         except:
             opt(red + "x", "Something in the setup when wrong.")
             raise
-
-    if(2 in user_options):
+'''
+    if (2 in user_options):
         name = input(r + "  [" + green + "*" + r +
                      "] Please enter your name (null): ")
         email = input(r + "  [" + green + "*" + r +
                       "] Please enter your email (N/A): ")
-        opt("*", "Please enter your projects description: ")
+        print(opt("*", "Please enter your projects description: "))
         print("  (This is my custom linux application)")
         description = input("  ")
+        '''
 
 
 def mainMenu():
     answer = ""
     global user_options
     try:
-        while(True):
+        while (True):
             os.system("clear")
             welcome()
             answer = input("\tPlease enter an input: ")
 
-            if(answer.lower() == "start"):
+            if (answer.lower() == "start"):
                 run()
                 break
             try:
                 user_options.append(int(answer))
             except:
-                print(opt(red+"!","ARE YOU RETARDED? Not a number!  ("+answer+")"))
+                print(opt(red + "!", "ARE YOU RETARDED? Not a number!  (" + answer + ")"))
                 raise KeyboardInterrupt()
 
     except KeyboardInterrupt:
