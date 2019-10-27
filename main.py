deca22bfa71f81dfae1df25411ad9ec372a73b3f
Lin8x <40849996+Lin8x@users.noreply.github.com>
2019-10-27 09:23:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
+#Update:
+# Make it so when the number is selected twice, its UNSELECTED
+# make it so when u answer all the information, it asks if all the information is correct, then does the animation
+# make it so all the * + - ! makes god damn sense:
+# # "*" Means proccess
+# # "!" Means for an error 
+# # "+" Means something is being added or listed (like the listed information when it asks if all the info is correct)
+# # "-" Means something really important is missing (like if a dependency isnt there)
+
+# Commentary to eric:
+# OMG WHY DID U ADD THE OPTIONS TO THE LIST CAUSE THE LOGO AND INFO IS SUPPOSED TO BE CALLED MULTIPLE TIMES, DO NOT COMBINE THE OPTIONS LIST WITH THE THING BEING SHOWED FOR ALMOST EVERY SCREEN AHHHHHH GOD DANG IT
+
 import os
 import time
 import sys
@@ -45,22 +57,17 @@ bwhite = '\33[107'
 
 # updates main menu options based on input
 
-
 def isSelected(value):
     if (value in user_options):
         return "X"
     return value
 
-
 # displays [value], string for main menu
-
 
 def opt(value, string):
     return r + "  [" + green + str(value) + r + "] " + string
 
-
 # displays the logo
-
 
 def welcome():
     os.system("clear " * 5)
@@ -80,43 +87,78 @@ def welcome():
   (L)inux (D)esktop (A)pplication (P)ackage (M)anager
 
   """ + r + "1st Developer: " + green + "lin8x" + r + " - " + ul + lcyan + "www.github.com/lin8x " + r +
-          "\n  2nd Developer: " + green + "asian-code" + r + " - " + ul + lcyan + "www.github.com/asian-code" + r +
-          "\n  Select the options you would like to use, then type 'start' to continue:\n\n" + opt(isSelected(1),
-                                                                                                   "Make a Desktop Application\n") +
-          opt(isSelected(2), "Make a .deb file (DOESN'T WORK ON NFTS!)\n") +
-          opt(isSelected(3), "Compile your name into a .tar.xz/.zip file\n"))
-
+          "\n  2nd Developer: " + green + "asian-code" + r + " - " + ul + lcyan + "www.github.com/asian-code" + r + "\n")
 
 def run():
+
+    #Shows the damn welcome screen cause eric seriously has no idea how to make a design for any tool
+    #NO ONE WANTS TO LOOK AT A LIST OF A BUNCH OF STUFF, MAKE IT BETTER
+
+    welcome()
+
     # bash file name
-    name = input(opt("*", "Enter app name : "))
+
+    name = input(opt("+", "Enter app name : "))
     if (name == ""):
         name = "projects"
+
     # folder location
+    # ERIC. you said "enter" instead of "Enter"...
+    # I swear I don't think you can even spell a sentence correctly, lol 
+    # Listen. I can understand a spelling mistake. BUT WHEN EVERY SENTENCE AROUND UR SENTENCE STARTS WITH A CAPITAL LETTER, U DID SOMETHING WRONG xD
+
     directory = input(
-        opt("*", "enter your projects name directory (~/mystuff): "))
+        opt("+", "Enter your projects name directory (~/mystuff): "))
     if (directory == ""):
         directory = "~/mystuff"
+
     # script to run/ write to bash file
+
     script = input(
-        opt("*", "Please enter your script's directory (~/mystuff.py): "))
+        opt("+", "Please enter your script's directory (~/mystuff.py): "))
     if (script == ""):
         script = "~/mystuff.py"
-    # need to talk to DANISGAY about what its purpose
+
+    #This is used to enter the command into the bash file so it nows how to rub the program
+
     runscript = input(opt(
-        "*", "Please enter the command in the terminal to run your tool\n (python3 mystuff.py)"))
+        "+", "Please enter the command in the terminal to run your tool\n (python3 mystuff.py)"))
+
+    #If 1 is isSelected
 
     if (1 in user_options):
+
         # get location of icon image
+
         image = input(
-            opt("*", "Please enter your image/logo's directory (home/mystuff.ico): "))
+            opt("+", "Please enter your image/logo's directory (home/mystuff.ico): "))
         if (image == ""):
             image = "~/mystuff.ico"
-        # determine if terminal runs when launching through desktop icon
+
+        # determine if terminal runs when launching through the desktop icon file
+
         terminal = input(
-            opt("*", "Is your software run on a terminal? (y/n): "))
+            opt("+", "Is your software run on a terminal? (y/n): "))
         terminal = (terminal.lower() == "y" or terminal.lower() == "yes")
+        print()
+
+        #asks user if all the information is correct (in case they mispelled something or whatever)
+
+        welcome()
+
+        answer = input(opt("+", "Is all the information here correct? (Y/n): "))
+
+        sum = opt(red + "*", "Getting everything ready for you")
+        for x in range(1, 4):
+          welcome()
+          sum = sum + "."
+          print(sum);
+          time.sleep(0.2)
+
         try:
+
+            welcome()
+
             # NEED TO CHECK FOR USER INPUT TO INCREASE APPLICATION SECURITY
             # app is using terminal commands which could be injected
             # make sure name and dir input are characters only
@@ -126,43 +168,45 @@ def run():
             # os.system("mkdir " + directory)
             # opt(red + "+", "Made " + directory)
 
-            # makes the parentfolder with subfolder (but whats the point of having a subfolder?)
             os.system("mkdir -p " + directory + "/" + projectdir)
-            print(opt(red + "+", "Made " + directory + "/" + name))
+            print(opt(red + "*", "Made " + directory + "/" + name))
 
             # copies the script file into the directory
+
             os.system("cp " + script + " " + directory + "/" + projectdir)
-            print(opt(red + "+", "Moved " + script +" to " + directory + "/" + projectdir))
+            print(opt(red + "*", "Moved " + script +" to " + directory + "/" + projectdir))
+
             # copies the icon file to subfolder
+
             os.system("cp " + image + " " + directory + "/" + projectdir)
             print(opt(red + "+", "Moved " + image +" to " + directory + "/" + projectdir))
 
             #create bash file
-            print(opt(red + "+", "Made the batch file called " + name))
+            print(opt(red + "*", "Made the batch file called " + name))
 
             file = open(name, "a")
             file.write(runscript)
             file.close()
-            #
-            os.system("mv " + name + " " + directory)
-            opt(red + "+", "Moved " + name + " to " + directory)
 
-            opt(red + "+", "Creating " + name + ".desktop (PRESS CNTRL + C)")
-            # DISGUSTING LIN8X CODE
-            #os.system("cat > " + name + ".desktop")
+            os.system("mv " + name + " " + directory)
+            opt(red + "*", "Moved " + name + " to " + directory)
+
+            opt(red + "*", "Creating " + name + ".desktop (PRESS CNTRL + C)")
             print()
-            opt(red + "+", "Created " + name + ".desktop")
+            opt(red + "*", "Created " + name + ".desktop")
 
             os.system("mv " + name + ".desktop " + directory)
-            opt(red + "+", "Moved " + name + ".desktop to " + directory)
+            opt(red + "*", "Moved " + name + ".desktop to " + directory)
 
             # make it for .desktop file and then write stuff to it
             # make it write stuff to the bat file too
             # https://formulae.brew.sh/formula/lynis#default
             # https://askubuntu.com/questions/90764/how-do-i-create-a-deb-package-for-a-single-python-script
 
+            #making stuff for loading screen plus asking if everything is ok
+
         except:
-            opt(red + "x", "Something in the setup when wrong.")
+            opt(red + "-", "Something in the setup when wrong.")
             raise
 '''
     if (2 in user_options):
@@ -183,7 +227,11 @@ def mainMenu():
         while (True):
             os.system("clear")
             welcome()
-            answer = input("\tPlease enter an input: ")
+            print(ul + "Select the options you would like to use, then type 'start' to continue:" + r + "\n")
+            print(opt(isSelected(1), "Make a Desktop Application"))
+            print(opt(isSelected(2), "Make a .deb file (DOESN'T WORK ON NFTS!)")) 
+            print(opt(isSelected(3), "Compile your name into a .tar.xz/.zip file\n"))
+            answer = input("  Please enter an input: ")
 
             if (answer.lower() == "start"):
                 run()
@@ -191,8 +239,8 @@ def mainMenu():
             try:
                 user_options.append(int(answer))
             except:
-                print(opt(red + "!", "ARE YOU RETARDED? Not a number!  (" + answer + ")"))
-                raise KeyboardInterrupt()
+                mainMenu()
+                break
 
     except KeyboardInterrupt:
         print(opt(red + "!", "Exiting..."))
